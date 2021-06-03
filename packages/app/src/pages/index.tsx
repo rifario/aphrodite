94e8da8af42b0bf03611686f71e43032666bfada
Abner Rodrigues <abnerluis1001@gmail.com>
2021-06-02 23:32:19 -0300

    feat(app): add dashboard page to index

--- packages/app/src/pages/index.tsx
+++ packages/app/src/pages/index.tsx
@@ -1,10 +1,70 @@
-import { Text, Heading } from '@chakra-ui/react'
+import { Box, Container, Heading, Stack } from '@chakra-ui/layout'
+import { CurrencyDollar, NoteBlank, Notebook } from 'phosphor-react'
 
-export default function Home(): JSX.Element {
+import { DataCard, OptionCard } from '@rifario/components/molecules'
+
+export default function Dashboard(): JSX.Element {
   return (
-    <div>
-      <Heading as="h1">Hello, world!</Heading>
-      <Text as="p">Programmed to work and not to feel.</Text>
-    </div>
+    <Container as="main" maxW="container.lg">
+      <Box
+        pt={32}
+        pb={48}
+        mx="calc(-50vw + 50%)"
+        bgGradient="linear(to-r, secondary.400, primary.400)"
+        as="header"
+      >
+        <Container maxW="container.lg">
+          <Heading mx="auto" as="h1" color="white">
+            Seja bem vindo, Fulano!
+          </Heading>
+        </Container>
+      </Box>
+      <Box as="section">
+        <Stack
+          as="header"
+          mt={-20}
+          mb={20}
+          spacing={6}
+          align="stretch"
+          justify="space-between"
+          direction={['column', 'column', 'column', 'row']}
+        >
+          <DataCard
+            icon={CurrencyDollar}
+            label="Faturamento"
+            value="R$ 23.000,00"
+          />
+          <DataCard icon={NoteBlank} label="Bilhetes vendidos" value="13.302" />
+          <DataCard icon={Notebook} label="Cartelas vendidos" value="7.685" />
+        </Stack>
+        <Box as="section">
+          <Heading mb={12} size="lg" fontWeight="bold">
+            Selecione o que deseja
+          </Heading>
+          <Stack
+            direction={['column', 'column', 'row']}
+            spacing={6}
+            align="stretch"
+            justifyContent="space-between"
+          >
+            <OptionCard
+              title="Meus tickets e cartelas"
+              description="Visualizar cartelas que você comprou"
+              href="#"
+            />
+            <OptionCard
+              title="Gerenciar Rifas"
+              description="Gerenciar as rifas que você criou"
+              href="#"
+            />
+            <OptionCard
+              title="Criar Nova Rifa"
+              description="Criar uma nova rifa"
+              href="#"
+            />
+          </Stack>
+        </Box>
+      </Box>
+    </Container>
   )
 }
