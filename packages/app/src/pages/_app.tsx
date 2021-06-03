c16a42790afb99caa896ba092603659c26dfd7fc
Abner Rodrigues <abnerluis1001@gmail.com>
2021-06-02 23:31:52 -0300

    feat(app): add Navbar to _app

--- packages/app/src/pages/_app.tsx
+++ packages/app/src/pages/_app.tsx
@@ -1,9 +1,16 @@
 import { AppProps } from 'next/app'
 import Provider from '@rifario/components/provider'
+import { Link } from '@rifario/components/atoms'
+import { Navbar } from '@rifario/components/molecules'
 
 export default function App({ Component, pageProps }: AppProps): JSX.Element {
   return (
     <Provider>
+      <Navbar bg="black" color="white" breakpoint="sm">
+        <Link href="#">
+          Olá, <strong>Fulano</strong>
+        </Link>
+      </Navbar>
       <Component {...pageProps} />
     </Provider>
   )
