447abb8d0546514a2e3969776563e8b3a5c794df
Abner Rodrigues <abnerluis1001@gmail.com>
2021-06-02 22:10:44 -0300

    feat(ui/molecules): add DataCard component

---
+++ packages/ui/molecules/DataCard.tsx
@@ -0,0 +1,40 @@
+import { As, Box, Flex, Heading, Icon } from '@chakra-ui/react'
+
+type DataCardProps = {
+  icon: As<unknown> | undefined
+  label: string
+  value: string
+}
+
+export default function DataCard({
+  icon,
+  label,
+  value
+}: DataCardProps): JSX.Element {
+  return (
+    <Box
+      w="full"
+      bg="white"
+      border="2px"
+      borderRadius="lg"
+      borderColor="primary.400"
+      px={10}
+      py={8}
+      as="article"
+    >
+      <Flex mb={5} align="center" as="header">
+        <Icon mr={2} w={4} h={4} as={icon} />
+        <Heading
+          letterSpacing="widest"
+          textTransform="uppercase"
+          fontWeight="regular"
+          as="h1"
+          size="sm"
+        >
+          {label}
+        </Heading>
+      </Flex>
+      <Heading as="h2">{value}</Heading>
+    </Box>
+  )
+}
