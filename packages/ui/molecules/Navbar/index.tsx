71f5500b6b0d73de4f77f49b7d0bb65c97c8a398
Abner Rodrigues <abnerluis1001@gmail.com>
2021-06-02 23:30:14 -0300

    feat(ui/molecules): add logo to Navbar

--- packages/ui/molecules/Navbar/index.tsx
+++ packages/ui/molecules/Navbar/index.tsx
@@ -1,5 +1,6 @@
 import { Flex, FlexProps, useDisclosure } from '@chakra-ui/react'
 import { ReactNode } from 'react'
+import { Link } from '../../atoms'
 
 import MenuLinks from './MenuLinks'
 import MenuToggle from './MenuToggle'
@@ -26,6 +27,9 @@ export default function Navbar({
       w="100%"
       p={8}
     >
+      <Link href="/dashboard" fontWeight="black">
+        Rifar.io
+      </Link>
       <MenuToggle breakpoint={breakpoint} isOpen={isOpen} toggle={onToggle} />
       <MenuLinks breakpoint={breakpoint} isOpen={isOpen}>
         {children}
