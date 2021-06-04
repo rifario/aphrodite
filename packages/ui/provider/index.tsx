c77c0a1f5e3f1710ae534e6a3059beed40c6910a
Abner Rodrigues <abnerluis1001@gmail.com>
2021-06-03 22:18:56 -0300

    feat(ui/provider): add light font weight for Poppins

--- packages/ui/provider/index.tsx
+++ packages/ui/provider/index.tsx
@@ -3,6 +3,7 @@ import { ReactNode } from 'react'
 
 import theme from '../services/chakra/theme'
 
+import '@fontsource/poppins/300.css'
 import '@fontsource/poppins/400.css'
 import '@fontsource/poppins/500.css'
 import '@fontsource/poppins/600.css'
