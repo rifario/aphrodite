1d32201136d2dd5b7fed63104b93db56e9c38220
Abner Rodrigues <abnerluis1001@gmail.com>
2021-06-03 22:32:16 -0300

    feat(app): add mock array to view data

--- packages/app/src/pages/raffles.tsx
+++ packages/app/src/pages/raffles.tsx
@@ -2,6 +2,39 @@ import { Box, Heading, Container, Grid } from '@chakra-ui/react'
 
 import { RaffleCard } from '@rifario/components/molecules/cards'
 
+const rafflesMock = [
+  {
+    id: 'a22HKl',
+    fresh: true,
+    title: 'Rifa de Dia dos Namorados',
+    prizeDrawDate: '10/06/2021',
+    data: {
+      ticketsAmount: 20000,
+      ticketsSold: 18337
+    }
+  },
+  {
+    id: 'x14PQS',
+    fresh: false,
+    title: 'Rifa de Natal',
+    prizeDrawDate: '24/12/2020',
+    data: {
+      ticketsAmount: 10000,
+      ticketsSold: 7923
+    }
+  },
+  {
+    id: 'e02XCQ',
+    fresh: true,
+    title: 'Bolão do 0KM',
+    prizeDrawDate: '10/10/2021',
+    data: {
+      ticketsAmount: 100000,
+      ticketsSold: 12350
+    }
+  }
+]
+
 export default function Raffles(): JSX.Element {
   return (
     <Container pt={20} maxW="container.lg" as="main">
@@ -16,17 +49,9 @@ export default function Raffles(): JSX.Element {
         templateColumns="repeat(auto-fill, minmax(15rem, 1fr))"
         autoRows="minmax(18rem, 1fr)"
       >
-        <RaffleCard
-          raffle={{
-            fresh: true,
-            title: 'Rifa de Natal',
-            prizeDrawDate: '14/08/2021',
-            data: {
-              ticketsAmount: 10000,
-              ticketsSold: 7923
-            }
-          }}
-        />
+        {rafflesMock.map(raffle => (
+          <RaffleCard key={raffle.id} raffle={raffle} />
+        ))}
       </Grid>
     </Container>
   )
