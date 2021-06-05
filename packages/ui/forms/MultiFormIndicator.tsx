49a84bc3247830de89e28ccbe2cc86d145ed3248
Abner Rodrigues <abnerluis1001@gmail.com>
2021-06-05 18:12:56 -0300

    feat(ui/forms): add MultiFormIndicator

---
+++ packages/ui/forms/MultiFormIndicator.tsx
@@ -0,0 +1,96 @@
+import { List, ListItem, Text, Circle, ListProps } from '@chakra-ui/layout'
+import { useBreakpointValue } from '@chakra-ui/media-query'
+import { chakra } from '@chakra-ui/system'
+import { Children, cloneElement, ReactComponentElement } from 'react'
+
+type MultiFormIndicatorItemProps = {
+  step?: number
+  label: string
+  current?: boolean
+}
+
+const MultiFormIndicatorItem = ({
+  step,
+  label,
+  current
+}: MultiFormIndicatorItemProps) => {
+  const circleSize = useBreakpointValue([5, 6, 8])
+  const fontSize = useBreakpointValue(['xs', 'sm'])
+  return (
+    <ListItem aria-labelledby={label} display="inline-block">
+      <Circle
+        data-current={current || undefined}
+        position="relative"
+        w={circleSize}
+        h={circleSize}
+        bgGradient="linear(to-b, secondary.400, primary.400)"
+        color="white"
+        fontWeight="bold"
+        mx="auto"
+        fontSize={fontSize}
+        transition="all 0.2s"
+        sx={{
+          '&[data-current]': {
+            transform: 'scale(1.1)',
+            boxShadow: '0 0 1px 2px rgba(89, 0, 255, 0.4)'
+          }
+        }}
+      >
+        {step}
+      </Circle>
+      <Text
+        aria-label={label}
+        mt={2}
+        fontWeight="semibold"
+        textAlign="center"
+        maxW="12ch"
+        isTruncated
+        fontSize={fontSize}
+      >
+        {label}
+      </Text>
+    </ListItem>
+  )
+}
+
+type MultiFormIndicatorProps = {
+  children: ReactComponentElement<typeof MultiFormIndicatorItem>[]
+}
+
+export default function MultiFormIndicator({
+  children,
+  ...rest
+}: MultiFormIndicatorProps & ListProps): JSX.Element {
+  const length = Children.count(children)
+  return (
+    <List
+      {...rest}
+      display="flex"
+      alignItems="start"
+      justifyContent="space-around"
+      as="ol"
+    >
+      {Children.map(children, (child, index) => (
+        <>
+          {index === length - 1 ? (
+            cloneElement(child, { step: index + 1 })
+          ) : (
+            <>
+              {cloneElement(child, { step: index + 1 })}
+              <chakra.hr
+                borderColor="black"
+                border="1px"
+                borderRadius="3xl"
+                w={`${((length * 10) / 100) * 100}%`}
+                h="full"
+                mt={7 / 2}
+              />
+            </>
+          )}
+        </>
+      ))}
+    </List>
+  )
+}
+
+MultiFormIndicator.Item = MultiFormIndicatorItem
